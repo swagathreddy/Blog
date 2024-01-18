5d4a749438a0dc334d32100c12f843f55222a1c3
Swagath reddy <125737432+swagathreddy@users.noreply.github.com>
2024-01-18 20:03:33 +0530

    app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 const express= require('express');
-const expressLayout =require("express-ejs-layouts");
+const expressLayouts =require("express-ejs-layouts");
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const connectDB=require("./server/config/db");
 const app=express();
-const PORT= process.env.PORT;
+const PORT= process.env.PORT  || 7595;
 
 
 connectDB();
@@ -14,7 +14,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static("public"));
 //templating  engine
-app.use(expressLayout);
+app.use(expressLayouts);
 app.set("layout","./layouts/main");
 app.set("view engine","ejs");
 app.use('/',require('./server/routes/main'));

468f1f247ed864a5e761a1bfc4927826cf6a0387
Swagath reddy <125737432+swagathreddy@users.noreply.github.com>
2024-01-18 17:51:33 +0530

    Post.js

--- server/models/Post.js
+++ server/models/Post.js
@@ -2,10 +2,6 @@ const mongoose=require("mongoose");
 const marked=require("marked");
 const Schema=mongoose.Schema;
 
-function formatDate(date) {
-    const options = { year: 'numeric', month: 'numeric', day: 'numeric', timeZone: 'Asia/Kolkata' };
-    return date.toLocaleDateString('en-IN', options);
-}
 const PostSchema=new Schema({
     title:{
         type:String,
